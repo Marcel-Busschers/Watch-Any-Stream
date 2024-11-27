d7e6f95e4a5de5770ff1ab9407d5d9caf936345b
Marcél Busschers <marcel.busschers@icloud.com>
2024-11-27 09:46:24 +0200

    Initial Commit

---
+++ Watch Any Stream/Watch_Any_StreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_Any_StreamApp.swift
+//  Watch Any Stream
+//
+//  Created by Marcél Busschers on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Any_StreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
